3c59dcbf0fddfa46a43d06b47284e92bd391f30b
Tom Walker <tom@filigree.tech>
2019-03-13 17:24:49 +0000

    fix

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
-    builder.add_common_builds()
+    #For c projects set pure_c to True to avoid building verisons for both libstdc++ and libc++
+    builder.add_common_builds(pure_c=False)
     builder.run()
