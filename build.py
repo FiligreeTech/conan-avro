400ffb47fb934ebf09527ddf18195404acbffa64
Tom Walker <tom@filigree.tech>
2019-03-13 17:36:38 +0000

    Remove shared builds

--- build.py
+++ build.py
@@ -5,4 +5,5 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(archs=["x86_64"])
     #For c projects set pure_c to True to avoid building verisons for both libstdc++ and libc++
     builder.add_common_builds(pure_c=False)
+    builder.remove_build_if(lambda build: build.options["avro:shared"])
     builder.run()
